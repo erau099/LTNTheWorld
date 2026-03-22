1865cde3a1b082844dd0fd4926845ca774175b03
444thien <kzoticc@gmail.com>
2026-03-21 20:02:23 -0700

    Sprint 1 DT-30: Finished header and banner for main page.

--- lovethyneighbor/src/App.jsx
+++ lovethyneighbor/src/App.jsx
@@ -25,18 +25,33 @@ function App() {
         <span className='header_title'>Lighten The World</span>
         <div className='header_links'>
           {NAV_LINKS.map((l) => (
-            <button key={l}>{l}</button>
+            <button key={l} className='headerbtn'>{l}</button>
           ))}
-          <button>Login</button>
-          <button>Sign Up</button>
+          <button className='loginbtn'>Login</button>
+          <button className='signupbtn'>Sign Up</button>
         </div>
         </nav>
+
+        {/* Banner */}
+        <section className='banner'>
+          <div className={`banner_bg ${visible ? "loaded" : ""}`}/>
+          <div className='banner_overlay'>
+          <div className={`banner_content ${visible ? "visible" : ""}`}>
+            <h1 className='banner_title'>
+              Share Extra Food<br/>
+              Help Your Community
+            </h1>
+            <div className='bannerbtn'>
+              <button className='btn1'>Find Food Near Me</button>
+              <button className='btn2'>Post Your Extra Food</button>
+            </div>
+          </div>
+          </div>
+        </section>
         
         <div style={{ height: '200vh', background: 'linear-gradient(to bottom, #f0f0f0, #ccc)' }}>
             <h1>Scroll down to see the magic!</h1>
         </div>
-
-        {/* Banner */}
 
 
       </div>
