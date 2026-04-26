bff214240fc330881cd78ab15212970ac77043ca
444thien <kzoticc@gmail.com>
2026-04-26 00:16:28 -0700

    Spring 4 DT-177: Tested form submission with Login.jsx and verified with hardcoded values from SpringBoot to ensure communication worked

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,50 @@
 import "./Login.css";
 import { useState, useEffect } from 'react'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login({}) {
+    // Store values
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [popup, setPopup] = useState('')
+    const navigate = useNavigate()
+
+    // Login function
+    const handleLogin = async (e) =>{
+        e.preventDefault()
+
+        // Send data to springboot as JSON to /api/auth/login
+        const response = await fetch ('/api/auth/login',{
+            method: 'POST',
+            headers: { 'Content-Type' : 'application/json'},
+            body: JSON.stringify({username, password})
+        })
+
+        const result = await response.text()
+
+        // Java returns either success or failure
+        if(result == 'success'){
+            setPopup('success')
+            setTimeout(() => navigate('/'), 1500)
+        } else{
+            setPopup('error')
+        }
+    }
+
     return(
         <div className="login_page">
+            {/* Popup only shows when popup is set to either success or error*/}
+            {popup === 'success' && (
+                <div className="popup success_popup">
+                    ✅ Signed in successfully! Redirecting...
+                </div>
+            )}
+            {popup === 'error' && (
+                <div className="popup error_popup">
+                    ❌ Incorrect username or password.
+                    <button onClick={() => setPopup('')}>✕</button>
+                </div>
+            )}
 
             {/*Header */}
             <nav className="nav">
@@ -20,10 +60,25 @@ function Login({}) {
             <section className="login_card">
                 <p className="login_label">Please Enter Your Name</p>
                 <h2 className="login_header">Sign In</h2>
-                <form className="login_info">
-                    <input type="email_address" placeholder="Email Address"></input>
-                    <input type="password" placeholder="Password"></input>
-                    <button className="submit_btn">Sign In</button>
+
+                {/*Submit form*/}
+                <form className="login_info" onSubmit = {handleLogin}>
+                    {/*Store texts*/}
+                    <input
+                        type = "text"
+                        placeholder = "Email Address"
+                        value = {username}
+                        onChange = {e => setUsername(e.target.value)}
+                    />
+                    <input
+                        type = "password"
+                        placeholder = "Password"
+                        value = {password}
+                        onChange = {e => setPassword(e.target.value)}
+                    />
+
+
+                    <button className="submit_btn" type="submit">Sign In</button>
                 </form>
                 <p className="not_registered">Not Registered?</p>
                 <div className="center_cancel_btn">
