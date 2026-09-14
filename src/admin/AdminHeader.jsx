d7236102a0f7c4a7bec23ec60fbcfac3c3072750
NathanAprieto <nathaniel.aprieto@gmail.com>
2026-09-13 22:11:49 -0700

    Sprint 5: DT-203 Connect and test admin page routing
    Connected the Admin Dashboard to the application routing.
    
    Changes made:
    - Added /admin-dashboard route
    - Added Home navigation to the admin header
    - Verified navigation between Admin Dashboard and Home
    - Tested Admin Dashboard components and user management actions
    - Verified the admin route works after refreshing

--- src/admin/AdminHeader.jsx
+++ src/admin/AdminHeader.jsx
@@ -1,20 +1,33 @@
+import { useNavigate } from "react-router-dom";
 import "../index.css";
 
 function AdminHeader() {
+  const navigate = useNavigate();
+
+    const handleHome = () => {
+    navigate("/");
+  };
+
   return (
     <nav className="nav">
       <span className="header_title">Admin Dashboard</span>
 
       <div className="header_links">
-        <button className="headerbtn">
+        <button
+          className="headerbtn"
+          onClick={handleHome}
+        >
+          Home
+        </button>
+
+        <button className="headerbtn" 
+        onClick={() => navigate("/admin-dashboard")}>
           Dashboard
         </button>
 
-        <button className="headerbtn">
-          User Management
-        </button>
 
-        <button className="gear_btn">
+        <button className="gear_btn"
+          onClick={() => navigate("/admin-settings")}>
           ⚙
         </button>
       </div>
